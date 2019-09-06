67b8dd6f1ffd4a7197bca6e0b8a7e6859da180cf
gitpanyu <1224721916@qq.com>
2019-09-06 18:07:12 +0800

    MyMacBook-Py 2019-9-6 6:07:12 PM

---
+++ index.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+_current_path() {
+    SOURCE=${BASH_SOURCE[0]}
+    DIR=$( dirname "$SOURCE" )
+    while [ -h "$SOURCE" ]
+    do
+        SOURCE=$(readlink "$SOURCE")
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+        DIR=$( cd -P "$( dirname "$SOURCE"  )" && pwd )
+    done
+    DIR=$( cd -P "$( dirname "$SOURCE" )" && pwd )
+    echo $DIR
+}
+echo $(_current_path)
+node $(_current_path)/dist/bundle.js
